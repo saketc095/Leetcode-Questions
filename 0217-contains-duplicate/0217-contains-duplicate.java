e888e0685f4d783cf6324e533843945d47391b18
Saket Chinchalikar <saketschinchalikar@gmail.com>
2026-04-30 19:01:12 -0700

    Time: 15 ms (64.84%), Space: 93.7 MB (31.71%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hs = new HashSet<>();
+        for(int num : nums){
+            if(hs.contains(num)){
+                return true;
+            }
+            hs.add(num);
+        }
+        return false;
+    }
+}
